df2bf20c293a06b3bf4ea6ba4975182d04ead62b
Chisaneko <david4sayt@gmail.com>
2023-03-23 22:10:59 -0300

    Done

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-num1 = 10;
 
-function teste(){
-    alert(num1)
+//var month = prompt("Em que mês estamos?");
+
+var month = parseInt(prompt("Which month we are"));
+
+switch(month){
+
+    case 1:
+        console.log("Estamos em Janeiro");
+        break;
+    
+    case 2:
+        console.log("Estamos em Fevereiro");
+        break;
+
+    case 3:
+        console.log("Estamos em Março");
+        break;
+    
+
+    default:
+        console.log("Mês não reconhecido");
+        break;
 }
